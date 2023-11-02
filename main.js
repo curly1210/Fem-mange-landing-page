4dfc7222cfb4093ac23004458213a31aea86fe4d
Cuong Pham <1210.curly@gmail.com>
2023-11-03 00:41:03 +0700

    add navigation

---
+++ main.js
@@ -0,0 +1,12 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', () => {
+  // primaryNav.hasAttribute('data-visible') 
+  // ? navToggle.setAttribute("aria-expaned", false)
+  // : navToggle.setAttribute("aria-expaned", true);
+
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute('data-overlay');
+})
