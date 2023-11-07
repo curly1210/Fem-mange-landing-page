7810167e4dcc503e3427b8a604f9e55fa02001c6
Cuong Pham <1210.curly@gmail.com>
2023-11-08 00:30:26 +0700

    add bloods

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener('click', () => {
-  // primaryNav.hasAttribute('data-visible') 
-  // ? navToggle.setAttribute("aria-expaned", false)
-  // : navToggle.setAttribute("aria-expaned", true);
+  primaryNav.hasAttribute('data-visible') 
+  ? navToggle.setAttribute("aria-expaned", false)
+  : navToggle.setAttribute("aria-expaned", true);
 
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute('data-overlay');
@@ -15,5 +15,5 @@ const slider = new A11YSlider(document.querySelector('.slider'), {
   adaptiveHeight: true,
   dots: true,
   arrows: false
-
+  
 });
